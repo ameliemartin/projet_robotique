1467f0be6dc8d415a9ec79805f72964db0fcaa51
CarlaPaill <82574717+CarlaPaill@users.noreply.github.com>
2021-05-11 20:00:12 +0200

    Ajout ControlRobot

--- projet/control_robot.h
+++ projet/control_robot.h
@@ -1,1 +1,7 @@
-// control robot fichier h 
+#ifndef CONTROL_ROBOT_H
+#define CONTROL_ROBOT_H
+
+//start the Control Robot thread
+void control_robot_start(void);
+
+#endif /* CONTROL_ROBOT_H*/// control robot fichier h 
