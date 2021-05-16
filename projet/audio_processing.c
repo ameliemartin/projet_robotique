8c6a5ca53574b983d5ed8af90af94628b11c920e
ameliemartin <amelie.martin@epfl.ch>
2021-05-16 10:13:16 +0200

    update micros

--- projet/audio_processing.c
+++ projet/audio_processing.c
@@ -123,22 +123,22 @@ void processAudioData(int16_t *data, uint16_t num_samples){
 	*/
 	int8_t turning_direction = 0; 
 	static uint16_t nb_samples = 0;
-		static uint8_t mustSend = 0;
+	static uint8_t mustSend = 0;
 
 		//loop to fill the buffers
 		for(uint16_t i = 0 ; i < num_samples ; i+=4){
 			//construct an array of complex numbers. Put 0 to the imaginary part
-			micRight_cmplx_input[nb_samples] = (float)data[i + MIC_RIGHT];
+			//micRight_cmplx_input[nb_samples] = (float)data[i + MIC_RIGHT];
 			micLeft_cmplx_input[nb_samples] = (float)data[i + MIC_LEFT];
-			micBack_cmplx_input[nb_samples] = (float)data[i + MIC_BACK];
-			micFront_cmplx_input[nb_samples] = (float)data[i + MIC_FRONT];
+			//micBack_cmplx_input[nb_samples] = (float)data[i + MIC_BACK];
+			//micFront_cmplx_input[nb_samples] = (float)data[i + MIC_FRONT];
 
 			nb_samples++;
 
-			micRight_cmplx_input[nb_samples] = 0;
+			//micRight_cmplx_input[nb_samples] = 0;
 			micLeft_cmplx_input[nb_samples] = 0;
-			micBack_cmplx_input[nb_samples] = 0;
-			micFront_cmplx_input[nb_samples] = 0;
+			//micBack_cmplx_input[nb_samples] = 0;
+			//micFront_cmplx_input[nb_samples] = 0;
 
 			nb_samples++;
 
@@ -155,10 +155,10 @@ void processAudioData(int16_t *data, uint16_t num_samples){
 			*	This is an "In Place" function.
 			*/
 
-			doFFT_optimized(FFT_SIZE, micRight_cmplx_input);
+			//doFFT_optimized(FFT_SIZE, micRight_cmplx_input);
 			doFFT_optimized(FFT_SIZE, micLeft_cmplx_input);
-			doFFT_optimized(FFT_SIZE, micFront_cmplx_input);
-			doFFT_optimized(FFT_SIZE, micBack_cmplx_input);
+			//doFFT_optimized(FFT_SIZE, micFront_cmplx_input);
+			//doFFT_optimized(FFT_SIZE, micBack_cmplx_input);
 
 		   /*	Magnitude processing
 			*
@@ -167,10 +167,10 @@ void processAudioData(int16_t *data, uint16_t num_samples){
 			*	real numbers.
 			*
 			*/
-			arm_cmplx_mag_f32(micRight_cmplx_input, micRight_output, FFT_SIZE);
+			//arm_cmplx_mag_f32(micRight_cmplx_input, micRight_output, FFT_SIZE);
 			arm_cmplx_mag_f32(micLeft_cmplx_input, micLeft_output, FFT_SIZE);
-			arm_cmplx_mag_f32(micFront_cmplx_input, micFront_output, FFT_SIZE);
-			arm_cmplx_mag_f32(micBack_cmplx_input, micBack_output, FFT_SIZE);
+			//arm_cmplx_mag_f32(micFront_cmplx_input, micFront_output, FFT_SIZE);
+			//arm_cmplx_mag_f32(micBack_cmplx_input, micBack_output, FFT_SIZE);
 
 			//sends only one FFT result over 10 for 1 mic to not flood the computer
 			//sends to UART3
